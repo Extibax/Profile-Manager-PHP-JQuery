5509b13208c28ea5e3bdc21dcfe9e54f27b9fa33
Extibax <extibax@gmail.com>
2019-04-27 19:26:25 -0500

    wip signup

--- php/signup.php
+++ php/signup.php
@@ -2,4 +2,9 @@
 
 if (isset($_POST['firstname']) && isset($_POST['username']) && isset($_POST['password'])) {
     
+
+    if ($_POST['firstname'] && $_POST['username'] && $_POST['password']) {
+        
+    }
+
 }
